c5ee4be7a3005dea55009cccf8df418393deacab
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-09-06 16:25:26 +0200

    kun site punkt gjenstår i 02_intents

--- 02_intents/src/main/java/com/example/a02_intents/MainActivity.java
+++ 02_intents/src/main/java/com/example/a02_intents/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a02_intents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(this::navigateToOtherActivity);
+        Button sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(this::navigateToOtherActivity);
+
+        Button webBtn = findViewById(R.id.webBtn);
+        webBtn.setOnClickListener(this::navigateToWebcontent);
+
+        findViewById(R.id.geoBtn).setOnClickListener(this::navigateToGeoLocation);
 
         text = findViewById(R.id.editText);
 
+    }
+
+    private void navigateToGeoLocation(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:59.128708,11.353176"));
+        startActivity(i);
+    }
+
+    private void navigateToWebcontent(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://www.google.no/?gws_rd=ssl"));
+        startActivity(i);
     }
 
     private void navigateToOtherActivity(View view) {
