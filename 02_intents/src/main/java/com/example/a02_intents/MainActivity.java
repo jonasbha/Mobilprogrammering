c6b1df4982ddc7be3bb7cfe42d9d57d126785a2b
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-09-06 17:38:06 +0200

    siste punkt bra i 02_intents. Trenger en shining

--- 02_intents/src/main/java/com/example/a02_intents/MainActivity.java
+++ 02_intents/src/main/java/com/example/a02_intents/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.a02_intents;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +37,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void navigateToOtherActivity(View view) {
+        Intent intent = new Intent(this, OtherActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, text.getText().toString());
+        mStartForResult.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> mStartForResult =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent intent = result.getData();
+                    // Handle the Intent
+                    TextView tV = findViewById(R.id.responseView);
+                    if (intent != null) {
+                        tV.setText(intent.getStringExtra(OtherActivity.EXTRA_REPLY));
+                    }
+                }
+            });
+
     private void navigateToGeoLocation(View view) {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse("geo:59.128708,11.353176"));
@@ -41,11 +66,5 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse("https://www.google.no/?gws_rd=ssl"));
         startActivity(i);
-    }
-
-    private void navigateToOtherActivity(View view) {
-        Intent intent = new Intent(this, OtherActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, text.getText().toString());
-        startActivity(intent);
     }
 }
--- 02_intents/src/main/java/com/example/a02_intents/OtherActivity.java
+++ 02_intents/src/main/java/com/example/a02_intents/OtherActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static String EXTRA_REPLY;
+    EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +23,17 @@ public class OtherActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+        mReply = findViewById(R.id.editTextSendBack);
+
+        findViewById(R.id.sendBackBtn).setOnClickListener(this::returnReply);
+    }
+
+    public void returnReply(View view){
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
