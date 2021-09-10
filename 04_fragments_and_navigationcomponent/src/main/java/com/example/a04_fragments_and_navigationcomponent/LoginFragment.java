69042456f51f6405bfccbe65442445ba14ca305b
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-09-10 17:07:56 +0200

    added fragments, nav- host, controller and graph

---
+++ 04_fragments_and_navigationcomponent/src/main/java/com/example/a04_fragments_and_navigationcomponent/LoginFragment.java
@@ -0,0 +1,35 @@
+package com.example.a04_fragments_and_navigationcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn = view.findViewById(R.id.loginBtn);
+        btn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_homeFragment));
+    }
+}
