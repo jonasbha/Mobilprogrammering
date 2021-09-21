e99de3fbb1b2469b99f45eff47fb1eb9f9e188a2
Jonas B. Halvorsen <jbh93hoved.annet@gmail.com>
2021-09-21 11:41:33 +0200

    venter på veiledning for alternativ setup av NavController

--- 07_navigation_ui/src/main/java/com/example/a07_navigation_ui/MainActivity.java
+++ 07_navigation_ui/src/main/java/com/example/a07_navigation_ui/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.a07_navigation_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+
+        NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.fragment_container);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
     }
 }
